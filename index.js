1169cd5b220536425bc04b6e5a7eaa65748f15cb
Your Brennan Predmore <brennnann@gmail.com>
2020-02-18 17:39:40 -0600

    axios npm init // string literal // finished_readme.md init

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const axios = require("axios");
 
 inquirer.prompt([{
             type: "input",
@@ -26,12 +27,14 @@ inquirer.prompt([{
         {
             type: "input",
             name: "install_dependencies",
-            message: "What command should be run to install dependencies?"
+            message: "What command should be run to install dependencies?",
+            default: "npm i"
         },
         {
             type: "input",
             name: "run_tests",
-            message: "What command should be run to run tests?"
+            message: "What command should be run to run tests?",
+            default: "npm test"
         },
         {
             type: "input",
@@ -56,3 +59,65 @@ inquirer.prompt([{
 
     })
 });
+
+function readMe() {
+    return `
+    
+# demo_day_project2
+[![GitHub license](https://img.shields.io/badge/license-GPL%203.0-blue.svg)](https://github.com/calvincarter/demo_day_project2)
+
+## Description
+
+the best project ever project
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${answer.install_dependencies}
+\`\`\`
+
+## Usage
+
+nothing just welcome all hands on deck
+
+## License
+
+This project is licensed under the GPL 3.0 license.
+  
+## Contributing
+
+please help
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+npm test
+\`\`\`
+
+## Questions
+
+<img src="https://avatars1.githubusercontent.com/u/4831868?v=4" alt="avatar" style="border-radius: 16px" width="30" />
+
+If you have any questions about the repo, open an issue or contact [calvincarter](https://api.github.com/users/calvincarter).
+
+
+    `;
+}
